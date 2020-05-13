23e75fc485d55ba5046a06c4bf6fa640c88d66aa
jim.meyer <no-reply>
2020-05-13 08:01:03 -0500

    Fixed flake8

--- example/example.py
+++ example/example.py
@@ -3,7 +3,7 @@ import json
 
 import keras
 from keras.datasets import cifar10
-#from keras.datasets import mnist
+from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
