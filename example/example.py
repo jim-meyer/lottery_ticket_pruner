1af583e1da0b6872d827624a2b6d91df0340c3b1
jim.meyer <no-reply>
2020-05-17 18:50:44 -0500

    Added cmd line arg support to example.py for key hyperparameters. Also hooked up ability to average results across runs.

--- example/example.py
+++ example/example.py
@@ -1,6 +1,8 @@
+import argparse
 import collections
 import copy
 import json
+import os
 
 import keras
 from keras.datasets import cifar10
@@ -15,8 +17,6 @@ import pandas as pd
 
 import lottery_ticket_pruner
 
-EvaluationResult = collections.namedtuple('EvaluationResult', 'loss accuracy')
-
 
 class LoggingCheckpoint(keras.callbacks.Callback):
     def __init__(self, experiment):
@@ -26,8 +26,9 @@ class LoggingCheckpoint(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         super().on_epoch_end(epoch, logs)
-        key = '{}_epoch_{}'.format(self.experiment, epoch)
-        self.epoch_data[key] = logs
+        if epoch not in self.epoch_data:
+            self.epoch_data[epoch] = collections.OrderedDict()
+        self.epoch_data[epoch][self.experiment] = logs
 
 
 class MNIST(object):
@@ -36,7 +37,7 @@ class MNIST(object):
         self.num_classes = 10
 
         # the data, split between train and test sets
-        func_map = {'mnist': self.load_mnist_data, 'cifar10': self.load_cifar10_data}
+        func_map = {'mnist': self.load_mnist_data, 'cifar10': self.load_cifar10_data, 'cifar10_reduced_10x': self.load_cifar10_reduced_10x_data}
         if which_set in func_map:
             (x_train, y_train), (x_test, y_test) = func_map[which_set]()
         else:
@@ -73,6 +74,12 @@ class MNIST(object):
         self.img_rows, self.img_cols = 32, 32
         self.channels = 3
 
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        return (x_train, y_train), (x_test, y_test)
+
+    def load_cifar10_reduced_10x_data(self):
+        (x_train, y_train), (x_test, y_test) = self.load_cifar10_data()
+
         def get_first_n(X, y, classes, n):
             result = []
             # Accumulate the first N samples of each class
@@ -85,9 +92,9 @@ class MNIST(object):
             y = y[result]
             return X, y
 
-        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-        x_train, y_train = get_first_n(x_train, y_train, range(self.num_classes), 500)
-        x_test, y_test = get_first_n(x_test, y_test, range(self.num_classes), 100)
+        # Reduce overall size of train, test sets by 10x
+        x_train, y_train = get_first_n(x_train, y_train, range(self.num_classes), x_train.shape[0] // (self.num_classes * 10))
+        x_test, y_test = get_first_n(x_test, y_test, range(self.num_classes), x_test.shape[0] // (self.num_classes * 10))
         return (x_train, y_train), (x_test, y_test)
 
     def load_mnist_data(self):
@@ -128,7 +135,12 @@ class MNIST(object):
         return loss, accuracy
 
     def get_epoch_logs(self):
-        """ Returns a dict of each epoch's accuracy, loss, validation accuracy, validation loss keyed by experiment name+epoch """
+        """ Returns a dict of each epoch's accuracy, loss, validation accuracy, validation loss.
+             result[<epoch>][<experiment>]['loss']
+             result[<epoch>][<experiment>]['acc']
+             result[<epoch>][<experiment>]['val_loss']
+             result[<epoch>][<experiment>]['val_acc']
+        """
         return self.logging_callback.epoch_data
 
 
@@ -147,50 +159,19 @@ class MNISTGloballyPruned(MNIST):
                   callbacks=callbacks)
 
 
-class MNISTNoDropout(MNIST):
-    def __init__(self, experiment, which_set='mnist'):
-        super().__init__(experiment, which_set=which_set)
-
-    def create_model(self):
-        model = Sequential()
-        model.add(Conv2D(32, kernel_size=(3, 3),
-                         activation='relu',
-                         input_shape=self.input_shape))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        # model.add(Dropout(0.25))
-        model.add(Flatten())
-        model.add(Dense(128, activation='relu'))
-        # model.add(Dropout(0.5))
-        model.add(Dense(self.num_classes, activation='softmax'))
-
-        model.compile(loss=keras.losses.categorical_crossentropy,
-                      optimizer=keras.optimizers.Adadelta(),
-                      metrics=['accuracy'])
-        return model
-
-
-class MNISTNoDropoutGloballyPruned(MNISTNoDropout):
-    def __init__(self, experiment, pruner, which_set='mnist'):
-        super().__init__(experiment, which_set=which_set)
-        self.pruner = pruner
-
-    def fit(self, model, epochs):
-        callbacks = self.callbacks + [lottery_ticket_pruner.PrunerCallback(self.pruner)]
-        model.fit(self.x_train, self.y_train,
-                  batch_size=self.batch_size,
-                  epochs=epochs,
-                  verbose=1,
-                  validation_data=(self.x_test, self.y_test),
-                  callbacks=callbacks)
-
-
-def compare():
-    epochs = 1
-
-    test_results = collections.defaultdict(dict)
-
-    which_set = 'mnist'
+def evaluate(which_set, prune_strategy, epochs, output_dir):
+    """ Evaluates multiple training approaches:
+            A model with randomly initialized weights evaluated with no training having been done
+            A model trained from randomly initialized weights
+            A model with randomly initialized weights evaluated with no training having been done *but* lottery ticket
+                pruning has been done prior to evaluation.
+            Several models trained from randomly initialized weights *but* with lottery ticket pruning applied at the
+                end of every epoch.
+        :returns losses and accuracies for the evaluations. Each are a dict of keyed by experiment name and whose value
+            is the loss/accuracy.
+    """
+    losses = {}
+    accuracies = {}
 
     experiment = 'MNIST'
     mnist = MNIST(experiment, which_set=which_set)
@@ -199,19 +180,12 @@ def compare():
     original_model = copy.deepcopy(model)
 
     experiment = 'MNIST_no_training'
-    test_results[experiment]['loss'], test_results[experiment]['accuracy'] = mnist.evaluate(model)
+    losses[experiment], accuracies[experiment] = mnist.evaluate(model)
 
     experiment = 'MNIST'
     mnist.fit(model, epochs)
-    test_results[experiment]['loss'], test_results[experiment]['accuracy'] = mnist.evaluate(model)
+    losses[experiment], accuracies[experiment] = mnist.evaluate(model)
     epoch_logs = mnist.get_epoch_logs()
-
-    # experiment = 'MNISTNoDropout'
-    # mnist_no_dropout = MNISTNoDropout(experiment, which_set=which_set)
-    # model = mnist_no_dropout.create_model()
-    # mnist_no_dropout.fit(model, epochs)
-    # test_results[experiment]['loss'], test_results[experiment]['accuracy'] = mnist_no_dropout.evaluate(model)
-    # epoch_logs.update(mnist_no_dropout.get_epoch_logs())
 
     # Use the weights from the trained model as the basis for determining what weights we'll prune for the new model.
     pruner = lottery_ticket_pruner.LotteryTicketPruner(model, original_model=original_model)
@@ -223,12 +197,12 @@ def compare():
         prune_rate = pow(prune_rate, 1.0 / (i + 1))
         overall_prune_rate = overall_prune_rate + prune_rate * (1.0 - overall_prune_rate)
 
-        pruner.prune_weights(prune_rate, 'smallest_weights_global')
+        pruner.prune_weights(prune_rate, prune_strategy)
         model.set_weights(starting_weights)
         pruner.apply_pruning()
 
         experiment = 'MNIST_no_training_pruned@{:.3f}'.format(overall_prune_rate)
-        test_results[experiment]['loss'], test_results[experiment]['accuracy'] = mnist.evaluate(model)
+        losses[experiment], accuracies[experiment] = mnist.evaluate(model)
 
     pruner.reset_masks()
 
@@ -239,31 +213,77 @@ def compare():
         prune_rate = pow(prune_rate, 1.0 / (i + 1))
         overall_prune_rate = overall_prune_rate + prune_rate * (1.0 - overall_prune_rate)
 
-        pruner.prune_weights(prune_rate, 'smallest_weights_global')
+        pruner.prune_weights(prune_rate, prune_strategy)
         model.set_weights(starting_weights)
         pruner.apply_pruning()
 
-        experiment = 'MNISTNoDropout_pruned@{:.3f}'.format(overall_prune_rate)
-        mnist_pruned = MNISTNoDropoutGloballyPruned(experiment, pruner, which_set=which_set)
+        experiment = 'MNIST_pruned@{:.3f}'.format(overall_prune_rate)
+        mnist_pruned = MNISTGloballyPruned(experiment, pruner, which_set=which_set)
         model = mnist_pruned.create_model()
         mnist_pruned.fit(model, epochs)
-        test_results[experiment]['loss'], test_results[experiment]['accuracy'] = mnist_pruned.evaluate(model)
-        epoch_logs.update(mnist_pruned.get_epoch_logs())
+        losses[experiment], accuracies[experiment] = mnist_pruned.evaluate(model)
+
+        epoch_logs2 = mnist_pruned.get_epoch_logs()
+        for epoch in epoch_logs.keys():
+            epoch_logs[epoch].update(epoch_logs2[epoch])
 
         # Periodically save the results to allow inspection during these multiple lengthy iterations
-        with open('epoch_logs.json', 'w') as f:
+        with open(os.path.join(output_dir, 'epoch_logs.json'), 'w') as f:
             json.dump(epoch_logs, f, indent=4)
-        with open('results.json', 'w') as f:
-            json.dump(test_results, f, indent=4)
 
-    epoch_logs_df = pd.DataFrame.from_dict(epoch_logs, orient='index')
-    epoch_logs_df.to_csv('epoch_logs.csv')
-    print(epoch_logs_df)
+    # Now save csv file so it's easier to compare loss, accuracy across the experiments
+    headings = []
+    for experiment in epoch_logs[0].keys():
+        headings.extend([experiment, '', '', ''])
+    sub_headings = ['train_loss', 'train_acc', 'val_loss', 'val_acc'] * len(epoch_logs[0])
+    epoch_logs_df = pd.DataFrame([], columns=[headings, sub_headings])
+    for epoch in range(len(epoch_logs)):
+        row = []
+        for experiment in epoch_logs[epoch].keys():
+            exp_dict = epoch_logs[i][experiment]
+            row.extend([exp_dict['loss'], exp_dict['acc'], exp_dict['val_loss'], exp_dict['val_acc']])
+        epoch_logs_df.loc[epoch] = row
+    epoch_logs_df.to_csv(os.path.join(output_dir, 'epoch_logs.csv'))
 
-    results_df = pd.DataFrame.from_dict(test_results, orient='index')
-    results_df.to_csv('results.csv')
-    print(results_df)
+    return losses, accuracies
 
 
 if __name__ == '__main__':
-    compare()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--iterations', type=int, required=True, default=1,
+        help='How many iterations to do. The final results will be averaged over these iterations.')
+    parser.add_argument('--epochs', type=int, required=False, default=20,
+        help='How many epochs to train for.')
+    parser.add_argument('--which_set', type=str, required=False, default='mnist',
+        help='Which data set to use. Must be one of "mnist", "cifar10" or "cifar10_reduced_10x". '
+             '"cifar10_reduced_10x" is the cifar10 data set with 10x fewer training, test samples. This is '
+             'useful for evaluating these pruning strategies using less data')
+    parser.add_argument('--prune_strategy', type=str, required=False, default='smallest_weights_global',
+        help='Which pruning strategy to use. Must be one of "random", "smallest_weights", "smallest_weights_global".'
+                        'See docs for LotteryTicketPruner.prune_weights() for full details.')
+    args = parser.parse_args()
+
+    for i in range(args.iterations):
+        output_dir = os.path.join(os.path.dirname(__file__), '{}_{}_{}_{}'.format(args.which_set, args.prune_strategy, args.epochs, 1))
+        os.makedirs(output_dir, exist_ok=True)
+        losses, accuracies = evaluate(args.which_set, args.prune_strategy, args.epochs, output_dir)
+
+        if i == 0:
+            # Only add headings once per every two sub-headings to reduce verbosity
+            headings = []
+            for key in losses.keys():
+                headings.extend([key, ''])
+            sub_headings = ['loss', 'acc'] * len(losses)
+            results_df = pd.DataFrame([], columns=[headings, sub_headings])
+        row = []
+        for key in losses.keys():
+            row.extend([losses[key], accuracies[key]])
+        results_df.loc[i] = row
+
+        results_df.to_csv(os.path.join(output_dir, 'results.csv'))
+        print(results_df)
+
+    mean = results_df.mean(axis=0)
+    results_df.loc['average'] = row
+    results_df.to_csv(os.path.join(output_dir, 'results.csv'))
+    print(results_df)
